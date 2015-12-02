43c997a603b9505f56b42d082593ea7ce342e2fa
UVERkill <vincentchen12@mittymonarch.com>
2015-12-01 19:43:16 -0800

    Upload animation project and assets
    Infinite GIF style animation of phone

---
+++ film170/animation/animation.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	console.log("Animation.JS Loaded");
+
+	var $img = $('#img');
+	
+	// currently works in 
+	$img.css("-webkit-animation", "rotate 3s infinite linear");
+});
