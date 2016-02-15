636d5f34bf44423cc5e78c8aa115d33d51e1158e
UVERkill <vincentchen12@mittymonarch.com>
2016-02-15 00:23:13 -0800

    update film html
    6th project added, wheel code added

--- assets/film170/film.js
+++ assets/film170/film.js
@@ -3,13 +3,22 @@ window.onload = function() {
 
 	var prev = 0;
 
+	if(top.location != location) {
+		top.location.href = document.location.href;
+	}
+
 	$('.proj-item').on("click", function(event) {
 		var val = $(this).attr("class").slice(-1);
-		if(val != prev) {
-			$("#projectwheel").removeClass("rotate"+prev);
-			$("#projectwheel").addClass("rotate"+val);
-			prev = val;
+		$("#projectwheel").removeClass("rotate"+prev);
+		$("#projectwheel").addClass("rotate"+val);
+		if(prev == 0) {
+			$("#proj"+val).fadeIn();
+		} else if(val != prev) {
+			$("#proj"+prev).fadeOut('fast', function() {
+				$("#proj"+val).fadeIn();
+			});
 		}
+		prev = val;
 	});
 
 }
